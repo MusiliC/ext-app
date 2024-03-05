80aa839e9709eaec00c1562f79dc524bba0fa8bc
MusiliC <musilibrian07@gmail.com>
2024-03-05 14:59:07 +0300

    Completed the online checkout form

--- app/view/layouts/CheckoutForm.js
+++ app/view/layouts/CheckoutForm.js
@@ -7,7 +7,8 @@ Ext.define("MyApp.view.layouts.CheckoutForm", {
   autoShow: true,
   draggable: false,
   modal: true,
-  layout: "fit",
+  //layout: "fit",
+  scrollable: 'y',
   items: [
     {
       xtype: "form",
@@ -138,9 +139,198 @@ Ext.define("MyApp.view.layouts.CheckoutForm", {
                 },
               ],
             },
+            {
+              xtype: "fieldset",
+              title: "Billing Address",
+              layout: "anchor",
+              defaults: {
+                anchor: "100%",
+              },
+              items: [
+                {
+                  xtype: "checkbox",
+                  boxLabel: "Same as Mailing Address?",
+                  name: "billingSameAsMailing",
+                  reference: "billingSameAsMailing",
+                  hideLabel: true,
+                  checked: true,
+                  margin: "0 0 10 0",
+                  handler: "onSameAddressChange",
+                },
+                {
+                  xtype: "textfield",
+                  fieldLabel: "Street Address",
+                  name: "billingStreet",
+                  reference: "billingStreet",
+
+                  labelWidth: 110,
+                  style: "opacity:.5",
+                  disabled: true,
+                  allowBlank: false,
+                },
+                {
+                  layout: "hbox",
+                  xtype: "container",
+                  margin: "0 0 5 0",
+                  items: [
+                    {
+                      xtype: "textfield",
+                      fieldLabel: "City",
+                      name: "mailingCity",
+                      reference: "mailingCity",
+                      flex: 1,
+                      allowBlank: false,
+                    },
+                    {
+                      xtype: "combobox",
+                      fieldLabel: "State",
+                      name: "billingState",
+                      reference: "billingState",
+                      width: 125,
+                      enforceMaxLength: true,
+                      style: "opacity:.5",
+                      labelWidth: 50,
+                      valueField: "abbr",
+                      displayField: "abbr",
+                      typeAhead: true,
+                      queryMode: "local",
+                      disabled: true,
+                      allowBlank: false,
+                      forceSelection: true,
+                      listConfig: {
+                        minWidth: null,
+                      },
+                    },
+                    {
+                      xtype: "textfield",
+                      fieldLabel: "Postal Code",
+                      name: "billingPostalCode",
+                      reference: "billingPostalCode",
+
+                      labelWidth: 80,
+                      style: "opacity:.5",
+                      width: 160,
+                      disabled: true,
+                      allowBlank: false,
+                      maxLength: 10,
+                      enforceMaxLength: true,
+                      maskRe: /[\d\-]/,
+                      regex: /^\d{5}(\-\d{4})?$/,
+                      regexText: "Must be in the format xxxxx or xxxxx-xxxx",
+                    },
+                  ],
+                },
+              ],
+            },
+            {
+              xtype: "fieldset",
+              title: "Payment",
+              layout: "anchor",
+              defaults: {
+                anchor: "100%",
+              },
+              items: [
+                {
+                  xtype: "radiogroup",
+                  layout: "hbox",
+                  defaults: {
+                    name: "ccType",
+                    margin: "0 15 0 0",
+                  },
+
+                  items: [
+                    {
+                      boxLabel: "VISA",
+                      inputValue: "visa",
+                      checked: true,
+                    },
+                    {
+                      boxLabel: "MasterCard",
+                      inputValue: "mastercard",
+                    },
+                    {
+                      boxLabel: "American Express",
+                      inputValue: "amex",
+                    },
+                    {
+                      boxLabel: "Discover",
+                      inputValue: "discover",
+                    },
+                  ],
+                },
+                {
+                  xtype: "textfield",
+                  fieldLabel: "Name On Card",
+                  name: "ccName",
+                  labelWidth: 110,
+                  allowBlank: false,
+                },
+                {
+                  xtype: "container",
+                  layout: "hbox",
+                  margin: "0 0 5 0",
+
+                  items: [
+                    {
+                      xtype: "textfield",
+                      fieldLabel: "Card Number",
+                      name: "ccNumber",
+                      labelWidth: 110,
+                      flex: 1,
+                      allowBlank: false,
+                      minLength: 15,
+                      maxLength: 16,
+                      enforceMaxLength: true,
+                      maskRe: /\d/,
+                    },
+                    {
+                      xtype: "fieldcontainer",
+                      fieldLabel: "Expiration",
+                      labelWidth: 75,
+                      layout: "hbox",
+                      items: [
+                        {
+                          xtype: "combobox",
+                          name: "ccExpireMonth",
+                          displayField: "name",
+                          valueField: "number",
+                          queryMode: "local",
+                          emptyText: "Month",
+                          hideLabel: true,
+                          margin: "0 6 0 0",
+                          width: 100,
+                          allowBlank: false,
+                          forceSelection: true,
+                        },
+                      ],
+                    },
+                    {
+                      xtype: "numberfield",
+                      name: "ccExpireYear",
+                      width: 90,
+                      hideLabel: true,
+                      value: new Date().getFullYear(),
+                      minValue: new Date().getFullYear(),
+                      allowBlank: false,
+                    },
+                  ],
+                },
+              ],
+            },
           ],
         },
       ],
+    },
+  ],
+  buttons: [
+    {
+      text: "Reset",
+      handler: "onResetClick",
+    },
+    {
+      text: "Complete Purchase",
+      width: 150,
+      handler: "onCompleteClick",
     },
   ],
 });
