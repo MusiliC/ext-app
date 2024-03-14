035061a023c56eff56edbac0b9238f0417361af1
MusiliC <musilibrian07@gmail.com>
2024-03-14 06:30:17 +0300

    Renderer error solved

--- app/util/Util.js
+++ app/util/Util.js
@@ -1,17 +1,20 @@
 Ext.define("MyApp.util.Util", {
-  statics: {
-    logInfo: function (message) {
-      console.log(message);
-    },
-  },
-  formatNumber: function (value) {
-    let v = new Number(value);
-    if (v > 0) {
-      let formatted = Ext.util.Format.number(value, "###,###.00");
-      return `<h3 style="color:green">${formatted}</h3>`;
-    } else {
-      let formatted = Ext.util.Format.number(value, "###,###.00");
-      return `<h3 style="color:red">${formatted}</h3>`;
+    statics:{
+        logInfo: function(message){
+            console.log(message)
+        },
+        formatNumber: function (value) {
+            let v = new Number(value);
+            if (v > 0) {
+                let formatted = Ext.util.Format.number(value, '###,###.00');
+                return `<h3 style="color:green">${formatted}</h3>`;
+
+            }
+            else {
+                let formatted = Ext.util.Format.number(value, '###,###.00');
+                return `<h3 style="color:red">${formatted}</h3>`;
+
+            }
+        }
     }
-  },
 });
